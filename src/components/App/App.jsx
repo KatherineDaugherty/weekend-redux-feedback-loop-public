f9626ff2b17f2d37effe5ac9eaeaa01b1ead0681
Katherine Daugherty <katherinedaugherty23@gmail.com>
2021-10-29 17:04:39 -0500

    added feeling component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 import Header from '../Header/Header.jsx';
+import Feeling from '../Feeling/Feeling.jsx';
 
 function App() {
 
   return (<>
     <Header />
+    <Feeling />
   </>
   )
 }
---
+++ src/components/Feeling/Feeling.jsx
@@ -0,0 +1,16 @@
+
+function Feeling() {
+
+    return (
+        <>
+<form>
+    
+<h1> How are you feeling today? </h1>
+<button>Next</button>
+
+
+</form>
+        </>
+    )
+}
+export default Feeling;
